7768c7522374870ca5110e2da401b8f3be30a7b7
Jonathon Martin <jonathon.martin@fotomerchant.com>
2021-09-07 22:12:00 +1000

    Fix build

--- middleware/toasts.tsx
+++ middleware/toasts.tsx
@@ -1,15 +1,12 @@
 import toast from 'react-hot-toast';
-import { Middleware } from 'redux';
 import UndoToast from '../components/undoToast';
 import { completeInstruction } from '../store/features/progress/progressSlice';
-import { RootState } from '../store/store';
 
-const ToastMiddleware: Middleware<{}, RootState> =
-  (storeApi) => (next) => (action) => {
-    if (action.type === completeInstruction.type) {
-      toast((t) => <UndoToast toastId={t.id} instructionId={action.payload} />);
-    }
-    return next(action);
-  };
+const UndoToastMiddleware = (storeApi: any) => (next: any) => (action: any) => {
+  if (action.type === completeInstruction.type) {
+    toast((t) => <UndoToast toastId={t.id} instructionId={action.payload} />);
+  }
+  return next(action);
+};
 
-export default ToastMiddleware;
+export default UndoToastMiddleware;
